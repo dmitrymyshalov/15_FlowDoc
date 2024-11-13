5f5c56a5465eedca39f5c116cb0ea4d6b6236405
dmitrymyshalov <dmitry.myshalov@bbraun.com>
2024-11-13 10:46:36 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Markup;
+
+namespace FlowDoc
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Open(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog
+            {
+                Filter = "Файлы разметки (*.xaml)|*.xaml"
+            };
+            if (dlg.ShowDialog() == true)
+            {
+                using (FileStream fs = File.Open(dlg.FileName, FileMode.Open))
+                {
+                    docViewer.Document = XamlReader.Load(fs) as FlowDocument;
+                }
+            }
+        }
+
+        private void Save(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog
+            {
+                Filter = "Файлы разметки (*.xaml)|*.xaml"
+            };
+            if (dlg.ShowDialog() == true)
+            {
+                using (FileStream fs = File.Open(dlg.FileName, FileMode.Create))
+                {
+                    XamlWriter.Save(docViewer.Document, fs);
+                }
+            }
+        }
+
+        private void Clear(object sender, RoutedEventArgs e)
+        {
+            docViewer.ClearValue(FlowDocumentScrollViewer.DocumentProperty);
+        }
+    }
+}
